03d374b474b2180c87bb5b202d5e032dbbe1087b
MelPeslier <mel.peslier@gmail.com>
2023-05-14 13:05:56 -0300

    rotation du joueur vers la souris 'smooth'

--- Player.gd
+++ Player.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 
 var max_speed :float = 500.0
 var acceleration :float = 375.0
-
+var rotation_speed :float = PI * 3.0
 
 func get_input():
 	if Input.is_action_pressed("move_up"):
@@ -31,6 +31,29 @@ func get_input():
 
 
 
-func _physics_process(_delta):
+func _physics_process(delta):
+	# Rotation douce entre le joueur et le viseur (la souris)
+	# On récupère le vecteur entre notre joueur et le viseur
+	var vec_to_aim = get_global_mouse_position() - global_position
+	
+	# On récupère l'angle entre cette nouvelle direction et la direstion actuelle
+	var angle_to_target = vec_to_aim.angle()
+	
+	# On récupère la rotation autorisée cette frame
+	var allowed_rotation = rotation_speed * delta
+	
+	# On récupère la rotation complète vers le viseur
+	angle_to_target = lerp_angle(global_rotation, angle_to_target, 1.0)
+	
+	# On limite la rotation à celle autorisée cete frame
+	angle_to_target = clamp(
+		angle_to_target,
+		global_rotation - allowed_rotation,
+		global_rotation + allowed_rotation
+	)
+	
+	# On définit la rotation du joueur !
+	global_rotation = angle_to_target
+	
 	get_input()
 	move_and_slide()
