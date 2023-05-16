d5e4bd3e7be55184d1300ca6b7ca7d62f83ec4e5
MelPeslier <mel.peslier@gmail.com>
2023-05-16 08:59:26 -0300

    petite restructuration pour garder les choses simples

--- Player/Player.gd
+++ Player/Player.gd
@@ -9,6 +9,31 @@ extends CharacterBody2D
 # Pour gérer l'accélération
 var input_time: float = 0.0
 
+func rotate_to_mouse(delta: float) -> void:
+	var rotation_speed: float = PI * 3.0
+	# Rotation douce entre le joueur et le viseur (la souris)
+	# On récupère le vecteur entre notre joueur et le viseur
+	var vec_to_aim = get_global_mouse_position() - global_position
+	
+	# On récupère l'angle entre cette nouvelle direction et la direstion actuelle
+	var angle_to_target = vec_to_aim.angle()
+	
+	# On récupère la rotation autorisée cette frame
+	var allowed_rotation = rotation_speed * delta
+	
+	# On récupère la rotation complète vers le viseur
+	angle_to_target = lerp_angle(global_rotation, angle_to_target, 1.0)
+	
+	# On limite la rotation à celle autorisée cette frame
+	angle_to_target = clamp(
+		angle_to_target,
+		global_rotation - allowed_rotation,
+		global_rotation + allowed_rotation
+	)
+	
+	# On définit la rotation du joueur !
+	global_rotation = angle_to_target
+
 func _ready() -> void:
 	states.init(self)
 
--- StatesMachines/base_state.gd
+++ StatesMachines/base_state.gd
@@ -11,33 +11,8 @@ enum State{
 
 var player: Player
 
-func rotate_to_mouse(delta: float) -> void:
-	var rotation_speed: float = PI * 3.0
-	# Rotation douce entre le joueur et le viseur (la souris)
-	# On récupère le vecteur entre notre joueur et le viseur
-	var vec_to_aim = player.get_global_mouse_position() - player.global_position
-	
-	# On récupère l'angle entre cette nouvelle direction et la direstion actuelle
-	var angle_to_target = vec_to_aim.angle()
-	
-	# On récupère la rotation autorisée cette frame
-	var allowed_rotation = rotation_speed * delta
-	
-	# On récupère la rotation complète vers le viseur
-	angle_to_target = lerp_angle(player.global_rotation, angle_to_target, 1.0)
-	
-	# On limite la rotation à celle autorisée cette frame
-	angle_to_target = clamp(
-		angle_to_target,
-		player.global_rotation - allowed_rotation,
-		player.global_rotation + allowed_rotation
-	)
-	
-	# On définit la rotation du joueur !
-	player.global_rotation = angle_to_target
-
-# Fonction pour gérer l'animation à ajouter
 func enter() -> void:
+# Pour gérer l'animation ; à ajouter
 	#player.animations.play(animation_name)
 	pass
 
@@ -51,5 +26,5 @@ func process(_delta: float) -> int:
 	return State.NULL
 
 func physics_process(delta: float) -> int:
-	rotate_to_mouse(delta)
+	player.rotate_to_mouse(delta)
 	return State.NULL
